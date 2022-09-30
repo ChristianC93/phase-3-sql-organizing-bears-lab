c2d9dd733cb6d1f92cc9a3cc1aebd6958bbdd88b
Christian Castillo <castillochristian3@gmail.com>
2022-09-30 10:01:34 -0400

    commit tree 746bb33a5d517ebc31621c91d031d94c2d316fa6

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'Black', 'Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'White', 'Sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'M', 'Brown', 'Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 9, 'F', 'Black', 'Sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 20, 'M', 'White', 'Phlegmatic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 1, 'F', 'Brown', 'Sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'M', 'Brown', 'Melancholic', 1);
